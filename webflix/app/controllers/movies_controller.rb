9488d488346ad499e0a71b0fe44014e7e72316fe
Jeff Cohen <cohen.jeff@gmail.com>
2015-04-30 20:20:13 -0500

    Use model associations to make our code awesome

--- webflix/app/controllers/movies_controller.rb
+++ webflix/app/controllers/movies_controller.rb
@@ -1,5 +1,12 @@
 class MoviesController < ApplicationController
 
+  before_action :find_movie, :only => [:show, :edit, :update, :destroy]
+  # before_action :find_movie, :except => [:index, :create, :new]
+
+  def find_movie
+    @movie = Movie.find_by(id: params["id"])
+  end
+
   def index
     if params["keyword"].present?
       @movies = Movie.where("title LIKE '%#{params["keyword"]}%'")
@@ -11,8 +18,6 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find_by(id: params["id"])
-
     if @movie == nil
       redirect_to movies_url, notice: "Movie not found."
     end
@@ -37,26 +42,23 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find_by(id: params["id"])
   end
 
   def update
-    movie = Movie.find_by(id: params["id"])
-    movie.title = params[:title]
-    movie.year = params[:year]
-    movie.runtime = params[:runtime]
-    movie.rated = params[:rated]
-    movie.stars = params[:stars]
-    movie.director_id = params[:director_id]
-    movie.plot = params[:plot]
-    movie.poster_url = params[:poster_url]
-    movie.save
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.runtime = params[:runtime]
+    @movie.rated = params[:rated]
+    @movie.stars = params[:stars]
+    @movie.director_id = params[:director_id]
+    @movie.plot = params[:plot]
+    @movie.poster_url = params[:poster_url]
+    @movie.save
     redirect_to movies_url
   end
 
   def destroy
-    movie = Movie.find_by(id: params["id"])
-    movie.delete
+    @movie.delete
     redirect_to movies_url
   end
 
--- webflix/app/models/movie.rb
+++ webflix/app/models/movie.rb
@@ -1,3 +1,10 @@
 class Movie < ActiveRecord::Base
 
+  has_many :roles
+  has_many :actors, :through => :roles
+
+  belongs_to :director
+
+  # belongs_to :director, class_name: 'Director', foreign_key: 'director_id'
+
 end
